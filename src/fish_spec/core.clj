57ab5c814b080b2826378d38baa3c9f6a2b72da3
John Stevenson <john@jr0cket.co.uk>
2016-05-31 17:54:49 +0100

    Specification for numbers data, valid? & explain
    Created a data structure for numbers and a complementary specification.
    The specification can then be used to check if statements are true and
    explain the details when false.

--- src/fish_spec/core.clj
+++ src/fish_spec/core.clj
@@ -1,6 +1,47 @@
-(ns fish-spec.core)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;  Description:
+;;  A clojure.spec example specifications that generate Dr. Seuss inspired rhymes,
+;;  based on http://gigasquidsoftware.com/blog/2016/05/29/one-fish-spec-fish/
+;;
+;;  Author: John Stevenson - @jr0cket
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns fish-spec.core
+  (:require [clojure.spec :as spec]))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; Create data structure for numbers
+
+;; Create a map with a finite number of fish numbers, this is a childs rhyme after all
+(def fish-numbers {0 "Zero"
+                   1 "One"
+                   2 "Two"})
+
+;; fish-numbers
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; Create specification for numbers
+
+;; use the spec/def to register the spec we are going to define for global reuse
+;; use a namespaced keyword ::fish-number to express that our specification for a valid number is the keys of the fish-numbers map
+(spec/def ::fish-number (set (keys fish-numbers)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; Test specification for numbers
+
+(spec/valid? ::fish-number 0)
+(spec/valid? ::fish-number 1)
+(spec/valid? ::fish-number 2)
+(spec/valid? ::fish-number 3)
+(spec/valid? ::fish-number 4)
+
+;; Some of the above tests fail, so we can ask for an explination why
+
+(spec/explain ::fish-number 4)
+
+;; in the repl the following is output:
+;; => val: 4 fails predicate: (set (keys fish-numbers))
